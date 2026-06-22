e5023dbfedb80df0eda2717f20dde3decf4ae7a7
Lincia Walters <lincia.walters@hotmail.com>
2026-06-22 18:48:58 -0400

    lecture zero cont'd

---
+++ lecture zero - functions and variables/hello.py
@@ -0,0 +1,47 @@
+
+# Ask user for their name
+name = input("What's your name? ") #input only accepts text to prompt user
+
+
+# Say hello to user 
+#rint("hello ," + name) #using operators does not add a space
+print("hello,", name) #when passing in 2 arguments, python puts the space for you 
+
+
+#print documentation 
+#print(*objects, sep=' ', end='\n', file=sys.stdout, flush=False) 
+
+
+#print adds a new line automatically
+#print("hello, ", end="") #this overrides the new line 
+#print(name)
+
+
+
+#python is not only a language, it's also a program that translates
+#human language code into ones and zeros for the computer - an intepreter
+#you can run programs by doing python *name of file* in the terminal 
+
+#functions
+#arguments
+#bugs & debugging 
+#return values and variables
+#operators; = is the assignment operator 
+#comments can be used as pseudocode; can use either the hash (#) or ("""") 
+
+
+"""
+this is a multi 
+line
+comment 
+"""
+
+#the best thing you can do is learn to read the documentation 
+
+#single and double quotes are interchangeable 
+
+
+
+
+
+
