42df6da69fb8b4830b8c3ce3357250b29d8f867c
Lincia Walters <lincia.walters@hotmail.com>
2026-06-29 05:22:48 -0400

    did some practice exercises

---
+++ lecture zero - functions and variables/practiceExercises.py
@@ -0,0 +1,119 @@
+# ======
+# write a function that adds two numbers
+# ======
+
+
+"""
+def sum():
+    x = input("Enter the first number: ")
+    y = input("Enter the second number: ")
+
+    z = int(x) + int(y)
+    print(f"The sum is {z}" )
+
+sum()
+"""
+
+
+# ======
+# write a function that subtracts two numbers
+# ======
+
+"""
+def subtract():
+    x = input("Enter the first number: ")
+    y = input("Enter the second number: ")
+
+    z = int(x) - int(y)
+    print(f"The difference is {z}" )
+
+subtract()
+"""
+
+# ======
+# write a function that multiplies two numbers
+# ======
+
+"""
+def multiply():
+    x = input("Enter the first number: ")
+    y = input("Enter the second number: ")
+
+    z = int(x) * int(y)
+    print(f"The product is {z}")
+
+multiply()
+"""
+
+# ======
+# write a function that calculates the area of a rectangle 
+# ======
+
+"""
+def area():
+    length = int(input("Enter the length: "))
+    width = int(input("Enter the width: "))
+
+    z = length * width
+    print(f"The area is {z}")
+
+area()
+"""
+
+# ======
+# write a function that calculates the circumference of a circle
+# ======
+
+"""
+def circumference():
+    pi = 3.14159
+    radius = float(input("Enter the radius: "))
+
+    total = 2 * pi * radius
+    print(f"The circumference is {total:.2f}")
+
+circumference()
+"""
+
+# ======
+# write a function that calculates celcius to fahrenheit
+# ======
+"""
+def ctof():
+    celsius = int(input("Enter temperature in Celsius: "))
+    fahrenheit = (celsius * 9 / 5) + 32
+
+    print(f"{celsius}°C = {fahrenheit}°F") # copied symbol from google
+
+ctof()
+"""
+
+
+# ======
+# write a function that calculates fahrenheit to celsius
+# ======
+
+
+# ======
+# write a function that calculates 15% tip on a bill
+# ======
+
+
+# ======
+# write a function that calculates 12.5% tax on a bill
+# ======
+
+
+# ======
+# write a function that calculates monthly savings
+# ======
+
+
+# ======
+# write a function that calculates loan interest
+# ======
+
+
+# ======
+# write a function that calculates BMI
+# ======
