2598b8e9d2334b2f7b04c3755468e57dc4d19b8f
Lincia Walters <lincia.walters@hotmail.com>
2026-06-29 05:40:50 -0400

    conditionals cont'd, elif else or

--- lecture one - conditionals/compare.py
+++ lecture one - conditionals/compare.py
@@ -16,14 +16,53 @@ y = int(input("What's y? "))
 
 # a boolean expression, named after someone named Bool 
 # this program will go through each conditional even if the first conditional is true
+"""
 if x < y: 
     print("x is less than y")
 if x > y:
     print("x is greater than y")
 if x == y:
     print("x is equal to y")
-
+"""
 
 # elif 
+"""
+if x < y: 
+    print("x is less than y")
+elif x > y:
+    print("x is greater than y")
+elif x == y:
+    print("x is equal to y")
+"""
+
+# else
+"""
+if x < y: 
+    print("x is less than y")
+elif x > y:
+    print("x is greater than y")
+else:
+    print("x is equal to y")
+"""
+
+# else is making less decisions; more efficient code
+
+
+# or
+if x < y or x > y:
+    print("x is not equal to y")
+else:
+    print("x is equal to y")
+
+
+# it's good to get into the habit of asking:
+# could my code be better?
+# could my code be simpler?
+# could i ask fewer questions?
+# could i tighten it up?
+
+
+
+
 
 
