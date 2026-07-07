eb7d9184d7bfc6a814e3d0a0904a4b7e1942d284
Lincia Walters <lincia.walters@hotmail.com>
2026-07-07 05:22:13 -0400

    finished with loops; while loopsm for loop and validating input using while

--- lecture two - loops/cat.py
+++ lecture two - loops/cat.py
@@ -93,17 +93,35 @@ print("meow")"""
 
 
 # alternatively:
-while True:
-    n = int(input("What's n? "))
-    if n > 0:
-        break 
+# while True:
+#     n = int(input("What's n? "))
+#     if n > 0:
+#         break 
 
-for _ in range(n): # don't forget: _ is a pythonic placeholder for i
-    print("meow")
+# for _ in range(n): # don't forget: _ is a pythonic placeholder for i
+#     print("meow")
     
 
+# defining a function, using while loop and range
+def main():
+    number = get_number()
+    meow(number)
 
 
+
+def get_number():
+    while True:
+        n = int(input("What's n? "))
+        if n > 0:
+            return n
+
+
+def meow(n):
+    for _ in range(n):
+        print("meow")
+
+
+main()
 
 
 
