05d9afb1585ca0f616ab8855341b33eb00fe5c91
Lincia Walters <lincia.walters@hotmail.com>
2026-07-07 18:59:26 -0400

    started dictionaries

--- lecture two - loops/hogwarts.py
+++ lecture two - loops/hogwarts.py
@@ -1,7 +1,7 @@
 
 # LISTS
 
-students = ["Hermione", "Harry", "Ron"]
+# students = ["Hermione", "Harry", "Ron"]
 
 # to go inside a list and print a specific value -
 # what you want to do is go inside and index the list like so:
@@ -30,8 +30,40 @@ students = ["Hermione", "Harry", "Ron"]
 
 
 # this will print a top 3
-for i in range(len(students)):
-    print(i + 1, students[i])
+# for i in range(len(students)):
+#     print(i + 1, students[i])
+
+
+
+
+# DICTIONARIES
+# key-value pairs
+students = {
+    "Hermione": "Gryffindor",
+    "Harry": "Gryffindor",
+    "Ron": "Gryffindor",
+    "Draco": "Slytherin"
+}
+
+# to print out specific indexes
+# print(students["Hermione"])
+# print(students["Harry"])
+# print(students["Ron"])
+# print(students["Draco"])
+
+
+# when you use a for loop to iterate over a dictionary,
+# it will print all keys
+# for student in students:
+#     print(student)
+
+
+# to get it to print key-value pairs, use an index identifier and the key 
+# for student in students:
+#     print(student, students[student], sep=", ")
+
+
+
 
 
 
