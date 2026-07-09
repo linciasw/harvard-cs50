fca5d194c684a490c554119ee6a589af33bc29a4
Lincia Walters <lincia.walters@hotmail.com>
2026-07-09 05:41:17 -0400

    same outcome, different code examples

---
+++ lecture two - loops/mario.py
@@ -0,0 +1,39 @@
+# TO PRINT 3 HASHES LIKE SO: (VERTICAL)
+#
+#
+#
+
+# for _ in range(3):
+#     print("#")
+
+
+
+# what's nice about functions is that you can change the underlying 
+# implementation details of them, but so long as you don't change the 
+# name of the function or its parameters, or what it returns, no one
+# else will know the difference
+
+
+# def main():
+#     print_column(3)
+
+# def print_column(height):
+#     for _ in range(height):
+#         print("#")
+
+# what's nice about functions is that you can change the underlying 
+# implementation details of them, but so long as you don't change the 
+# name of the function or its parameters, or what it returns, no one
+# else will know the difference
+
+# for example, we can change the above to the below
+# it does the same exact thing but different code
+# def print_column(height):
+#     print("#\n" * height, end="")
+
+
+
+# TO PRINT 3 HASHES LIKE SO: (HORIZONTAL)
+###
+
+main()
