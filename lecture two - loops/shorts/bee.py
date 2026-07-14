2b1ba31d98c3547c127bdec914560924e351db92
Lincia Walters <lincia.walters@hotmail.com>
2026-07-14 18:42:11 -0400

    dictionaries methods completed

--- lecture two - loops/shorts/bee.py
+++ lecture two - loops/shorts/bee.py
@@ -1,8 +1,8 @@
 # DICTIONARY METHODS
 
-WORDS = {"PAIR": 4, "HAIR": 4, "CHAIR": 5}
+WORDS = {"PAIR": 4, "HAIR": 4, "CHAIR": 5, "GRAPHIC": 7}
 
-
+"""
 def main():
     print("Welcome to Spelling Bee!")
     print("Your letters are: A I P C R H G")
@@ -13,6 +13,12 @@ def main():
 
 
         # TODO: Check if guess in dictionary
+        # to clear the entire dictionary
+        if guess == "GRAPHIC":
+            WORDS.clear()
+            print("You've won!")
+
+
         if guess in WORDS.keys():
             # print(f"Good job! You scored {WORDS[guess]} points.") # this will access the value associated with the key
 
@@ -22,6 +28,24 @@ def main():
 
 
     print("That's the game!")
+
+
+main()
+"""
+
+
+
+def main():
+    print("Welcome to Spelling Bee!")
+
+
+    
+    for word, points in WORDS.items():
+        print(f"{word} was worth {points} points.")
+
+
+
+
 
 
 main()
