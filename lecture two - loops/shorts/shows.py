d9d056fe587c9740cc88cc489c3f642294ceffb3
Lincia Walters <lincia.walters@hotmail.com>
2026-07-22 06:02:35 -0400

    string methods short

--- lecture two - loops/shorts/shows.py
+++ lecture two - loops/shorts/shows.py
@@ -17,8 +17,17 @@ SHOWS = [
 
 
 def main():
+    cleaned_shows = []
     for show in SHOWS:
-        print(show)
+        # print(show.capitalize()) # only uppercases the first character in your string 
+        # print(show.title()) # capitzalizes all words in string
+        # print(show.strip()) # strips leading & trailing spaces
+        # print(show.strip().title()) # methods can be chained
+        cleaned_shows.append(show.strip().title())
 
+    #print(cleaned_shows)
+    # output: ['Avatar: The Last Airbender', 'Ben 10', 'Arthur', 'Spongebob Squarepants', 'Phineas And Ferb', 'Kim Possible', 'Jimmy Neutron', 'The Proud Family']
+
+    print(', '.join(cleaned_shows))
 
 main()
