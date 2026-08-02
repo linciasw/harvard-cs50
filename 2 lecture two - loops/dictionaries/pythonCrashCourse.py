7d727f71cca17e207c4f956d12430c1e330c9755
Lincia Walters <lincia.walters@hotmail.com>
2026-08-02 08:46:17 -0400

    chapter 6 python crash course notes - dictionaries

---
+++ 2 lecture two - loops/dictionaries/pythonCrashCourse.py
@@ -0,0 +1,32 @@
+# CHAPTER 6
+# DICTIONARIES
+
+
+'''
+a dictionary in python is a collection of key-value pairs.
+each key is connected to a value, and you can use a key to access 
+the value associated with that key.
+a key's value can be a number, a string, a list, or even another dictionary.
+you can use any object that you can create in python as a value in a dictionary.
+'''
+
+alien_0 = {"color": "green", "points": "5"}
+
+
+'''
+a key-value pair is a set of values associated with each other. 
+when you provide a key, python returns the value associated with that key.
+'''
+
+
+# to get the value associated with a key, give the name of the dictionary 
+# and then place the key inside a set of square brackets as shown below.
+# the output of this is green
+print(alien_0["color"])
+
+
+
+
+
+
+
