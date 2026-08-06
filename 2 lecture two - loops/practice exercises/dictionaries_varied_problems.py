ca8e6b3c57bb99d38e6aa7f3593b91a085506e33
Lincia Walters <lincia.walters@hotmail.com>
2026-08-06 14:44:58 -0400

    update on last exercise

--- 2 lecture two - loops/practice exercises/dictionaries_varied_problems.py
+++ 2 lecture two - loops/practice exercises/dictionaries_varied_problems.py
@@ -481,6 +481,29 @@ response = {
 
 
 # 3. Print each forecast day.
+forecast = response["forecast"]
+print(forecast)
+
+for day in forecast:
+    print(day)
+
+
+# 4. Find the hottest forecast.
+max_value = 0
+temp = forecast["temp"]
+
+print(forecast["temp"])
+
+for day in forecast:
+    if temp > max_value:
+        max_value = temp
+
+
+print(max_value)
+
+
+
+
 
 
 
