d72ead6b00a548c43eb5d459a969bfa7d90ea9dc
Lincia Walters <lincia.walters@hotmail.com>
2026-08-07 18:57:34 -0400

    entire program entered in

--- 3 lecture three - exceptions/shorts/distances.py
+++ 3 lecture three - exceptions/shorts/distances.py
@@ -3,7 +3,7 @@
 
 
 distances  = {
-    "Voyager 1": "163"
+    "Voyager 1": "163",
     "Voyager 2": "136",
     "Pioneer 10": "80 AU",
     "New Horizons": "58",
@@ -15,3 +15,11 @@ def main():
     spacecraft = input("Enter a spacecraft: ")
     m = convert(distances[spacecraft])
     print(f"{m} m away")
+
+
+
+def convert(au):
+    return au * 149597870700
+
+
+main()
