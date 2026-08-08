54451ecd3d47ba3e727ca69874647fbcb72b364b
Lincia Walters <lincia.walters@hotmail.com>
2026-08-08 08:06:04 -0400

    handling exceptions short

--- 3 lecture three - exceptions/shorts/distances.py
+++ 3 lecture three - exceptions/shorts/distances.py
@@ -11,9 +11,31 @@ distances  = {
 }
 
 
+'''
+choosing Pioneer 2 would give a:
+"ValueError: could not convert string to float: '80 AU'".
+a try-except block would help.
+if the user chooses a spacecraft that's not on the list, that will give a :
+"KeyError: 'James Webb Space Telscope'"
+
+as you write code, try to anticipate these errors, and write code to 
+handle them. 
+it's good practice to be as specific as possible with the errors.
+'''
+
 def main():
     spacecraft = input("Enter a spacecraft: ")
-    m = convert(distances[spacecraft])
+
+    try:
+        au = float(distances[spacecraft])
+    except KeyError:
+        print(f"'{spacecraft}' is not in dictionary")
+        return
+    except ValueError:
+        print(f"Can't convert '{distances[spacecraft]}' to a float")
+        return
+
+    m = convert(au)
     print(f"{m} m away")
 
 
