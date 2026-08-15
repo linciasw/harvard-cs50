d4a301f0e8cfb29eff114caac5eca941e835d533
Lincia Walters <lincia.walters@hotmail.com>
2026-08-15 17:32:56 -0400

    finished the sys module

--- 4 lecture four - libraries/lecture/name.py
+++ 4 lecture four - libraries/lecture/name.py
@@ -4,6 +4,25 @@
 # command-line arguments
 # the sys module 
 
+'''
+>>> dir(sys)
+['__breakpointhook__', '__displayhook__', '__doc__', '__excepthook__', '__interactivehook__', '__loader__', '__name__', '__package__', 
+ '__spec__', '__stderr__', '__stdin__', '__stdout__', '__unraisablehook__', '_base_executable', '_baserepl', '_clear_internal_caches', 
+ '_clear_type_cache', '_clear_type_descriptors', '_current_exceptions', '_current_frames', '_debugmallocstats', '_dump_tracelets', 
+ '_enablelegacywindowsfsencoding', '_framework', '_get_cpu_count_config', '_getframe', '_getframemodulename', '_git', '_home', '_is_gil_enabled', 
+ '_is_immortal', '_is_interned', '_jit', '_setprofileallthreads', '_settraceallthreads', '_stdlib_dir', '_vpath', '_xoptions', 
+ 'activate_stack_trampoline', 'addaudithook', 'api_version', 'argv', 'audit', 'base_exec_prefix', 'base_prefix', 'breakpointhook', 
+ 'builtin_module_names', 'byteorder', 'call_tracing', 'copyright', 'deactivate_stack_trampoline', 'displayhook', 'dllhandle', 
+ 'dont_write_bytecode', 'exc_info', 'excepthook', 'exception', 'exec_prefix', 'executable', 'exit', 'flags', 'float_info', 'float_repr_style', 
+ 'get_asyncgen_hooks', 'get_coroutine_origin_tracking_depth', 'get_int_max_str_digits', 'getallocatedblocks', 'getdefaultencoding', 
+ 'getfilesystemencodeerrors', 'getfilesystemencoding', 'getprofile', 'getrecursionlimit', 'getrefcount', 'getsizeof', 'getswitchinterval', 
+ 'gettrace', 'getunicodeinternedsize', 'getwindowsversion', 'hash_info', 'hexversion', 'implementation', 'int_info', 'intern', 'is_finalizing', 
+ 'is_remote_debug_enabled', 'is_stack_trampoline_active', 'maxsize', 'maxunicode', 'meta_path', 'modules', 'monitoring', 'orig_argv', 'path', 
+ 'path_hooks', 'path_importer_cache', 'platform', 'platlibdir', 'prefix', 'ps1', 'ps2', 'pycache_prefix', 'remote_exec', 'set_asyncgen_hooks', 
+ 'set_coroutine_origin_tracking_depth', 'set_int_max_str_digits', 'setprofile', 'setrecursionlimit', 'setswitchinterval', 'settrace', 'stderr', 
+ 'stdin', 'stdlib_module_names', 'stdout', 'thread_info', 'unraisablehook', 'version', 'version_info', 'warnoptions', 'winver']
+ '''
+
 
 import sys
 
@@ -38,10 +57,64 @@ print(sys.argv)
 # worried about.
 # this way, we can also instruct the user a little better
 # we can't have multiple else statemets
+# if len(sys.argv) < 2:
+#     print("Too few arguments")
+# elif len(sys.argv) > 2:
+#     print("Too many arguments")
+# else:
+#     print("hello, my name is", sys.argv[1])
+
+
+
+# if we remove the else like the below, to separate different parts of the code
+# we'll get an IndexError if nothing is inputted because the print statement will 
+# always try to go for the first index despite us checking the length in the conditional
+# Check for errors
+# if len(sys.argv) < 2:
+#     print("Too few arguments")
+# elif len(sys.argv) > 2:
+#     print("Too many arguments")
+
+# Print name tags
+# print("hello, my name is", sys.argv[1])
+
+
+
+
+# to fix the above bug, we can use sys.exit as below
+# Check for errors
+# if len(sys.argv) < 2:
+#     sys.exit("Too few arguments")
+# elif len(sys.argv) > 2:
+#     sys.exit("Too many arguments")
+
+# Print name tags
+# print("hello, my name is", sys.argv[1])
+
+
+
+
+# this is a way to print more than one name inputted
+# this prints the program name (name.py) store at index 0 though
+# Check for errors
+# if len(sys.argv) < 2:
+#     sys.exit("Too few arguments")
+
+# Print name tags
+# for arg in sys.argv:
+#     print("hello, my name is", arg)
+
+
+
+
+# to remove index 0, name of the program, we can use slicing as below
+# SLICING
+# Check for errors
 if len(sys.argv) < 2:
-    print("Too few arguments")
-elif len(sys.argv) > 2:
-    print("Too many arguments")
-else:
-    print("hello, my name is", sys.argv[1])
+    sys.exit("Too few arguments")
+
+# Print name tags
+for arg in sys.argv[1:]:
+    print("hello, my name is", arg)
+
 
