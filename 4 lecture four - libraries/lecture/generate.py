e680c104dc70493e43a4ef25e6a50fac3a30917e
Lincia Walters <lincia.walters@hotmail.com>
2026-08-15 09:23:08 -0400

    generate.py created, lecture notes

---
+++ 4 lecture four - libraries/lecture/generate.py
@@ -0,0 +1,51 @@
+# FROM LECTURE
+
+'''
+libraries are files of code that other people have written that
+you can use in your own's program or a library's code that you have
+written that you can use in your own program  
+
+it does this by using modules.
+a module in python is just a library that typically has one or more
+functions or other features built into it.
+the purpose of it is to encourage reusability of code.
+if you find yourself constantly copying code from old projects into new
+ones, there's an opportunity to create a module to load into programs.
+
+
+a module is a singly python file (.py) containing reusable code, a package is a 
+folder containing multiple related modules, while a library is a collection of related 
+packages bundled together to provide broader functionality. 
+
+
+example of modules
+- random 
+- math
+- datetime
+
+documentation
+https://docs.python.org/3/library/random.html#module-random
+'''
+
+# to import everything in the module
+# import random 
+
+
+# because we have imported everything from the module, we'll have
+# to specify the method by using random.choice()
+# coin = random.choice(["heads", "tails"])
+# print(coin)
+
+
+# if we want to just use the choice function, we do 
+from random import choice
+
+coin = choice(["heads", "tails"])
+print(coin)
+
+
+
+
+
+
+
