fec78509e880a1773ca5db5fb8a8223c4571bf00
Lincia Walters <lincia.walters@hotmail.com>
2026-08-20 10:36:02 -0400

    students.py file created, style short

---
+++ 4 lecture four - libraries/shorts/students.py
@@ -0,0 +1,15 @@
+# FROM SHORTS
+# REACHED 8:57 
+
+
+
+# PEP8
+# python enhancement proposal
+# if your code is not readable or properly formatted, you run the risk of introducing bugs 
+# peps.python.org/pep-0008
+# pylint, a library to help with formatting
+# pylint is noisy, overwhelming
+# pycodestyle, pycodestyle.pycqa.org; will fix your formatting for you
+# black is another tool, black.readthedocs.io
+# 
+
