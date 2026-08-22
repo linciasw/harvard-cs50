55b1eef6a0a8c2035b90d8a73972166af9ff1b4a
Lincia Walters <lincia.walters@hotmail.com>
2026-08-22 09:48:33 -0400

    worked on figlet.py

--- 4 lecture four - libraries/problem sets/figlet.py
+++ 4 lecture four - libraries/problem sets/figlet.py
@@ -1,4 +1,10 @@
 # find out why -f not working if i put in the command line
+# documentation for figlet
+# https://www.figlet.org/figlet-man.html
+
+
+# documentation for sys
+# https://docs.python.org/3/library/sys.html
 
 from pyfiglet import Figlet
 import sys
@@ -18,8 +24,24 @@ if len(sys.argv) == 1:
     print(figlet.renderText(input))
 elif len(sys.argv) == 2:
     # font = sys.argv[2]
-    figlet.setFont(font=sys.argv[2])
+    figlet.setFont(font=sys.argv[1])
     print(figlet.renderText(input))
+elif len(sys.argv) == 3 and sys.argv[1] != "-f" or sys.argv[1] != "-font":
+    sys.exit
+
+
+# else:
+#     sys.exit
+
+
+print(len(sys.argv))
+# print(sys.argv[1])
+# print(sys.argv[2])
+# print(sys.argv[3])
+
+
+
+
 
 
 
