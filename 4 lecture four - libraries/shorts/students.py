136692792f8bfea834c6e234578f178765ad9f41
Lincia Walters <lincia.walters@hotmail.com>
2026-08-22 09:21:41 -0400

    style short finished

--- 4 lecture four - libraries/shorts/students.py
+++ 4 lecture four - libraries/shorts/students.py
@@ -1,15 +1,36 @@
 # FROM SHORTS
-# REACHED 8:57 
-
+# REACHED 8:57
 
 
 # PEP8
 # python enhancement proposal
-# if your code is not readable or properly formatted, you run the risk of introducing bugs 
+# if your code is not readable or properly formatted, you run the risk of introducing bugs
 # peps.python.org/pep-0008
 # pylint, a library to help with formatting
 # pylint is noisy, overwhelming
 # pycodestyle, pycodestyle.pycqa.org; will fix your formatting for you
 # black is another tool, black.readthedocs.io
-# 
+
+# DOING:
+# black students.py 
+# on the terminal will format the below dictionary
+
+# students = {
+#     "Hermione": "Gryffindor", "Harry": "Gryffindor", "Ron": "Gryffindor", "Draco": "Slytherin"}
+
+# for student in students:
+#     print(student)
+
+
+students = {
+    "Hermione": "Gryffindor",
+    "Harry": "Gryffindor",
+    "Ron": "Gryffindor",
+    "Draco": "Slytherin",
+}
+
+for student in students:
+    print(student)
+
+
 
