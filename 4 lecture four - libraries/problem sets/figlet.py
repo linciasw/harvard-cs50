bb9732639b1165c630b37f0a20120a5ea41b220d
Lincia Walters <lincia.walters@hotmail.com>
2026-08-22 08:58:00 -0400

    worked on figlet.py problem set

--- 4 lecture four - libraries/problem sets/figlet.py
+++ 4 lecture four - libraries/problem sets/figlet.py
@@ -1,1 +1,27 @@
-# 
+# find out why -f not working if i put in the command line
+
+from pyfiglet import Figlet
+import sys
+import random 
+
+
+figlet = Figlet()
+
+
+input = input("Input: ")
+
+
+
+
+if len(sys.argv) == 1:
+    figlet.setFont(font=str(random.choice(figlet.getFonts())))
+    print(figlet.renderText(input))
+elif len(sys.argv) == 2:
+    # font = sys.argv[2]
+    figlet.setFont(font=sys.argv[2])
+    print(figlet.renderText(input))
+
+
+
+
+
