d5454f95c67b0ef19201b5b64561894404c4b558
Lincia Walters <lincia.walters@hotmail.com>
2026-08-23 13:34:55 -0400

    game.py completed

--- 4 lecture four - libraries/problem sets/game.py
+++ 4 lecture four - libraries/problem sets/game.py
@@ -3,3 +3,46 @@
 import random 
 
 
+while True:
+    try:
+        n = int(input("Level: "))
+        number = random.randint(1, n)
+    except ValueError:
+        continue
+    else:
+        break
+
+
+while True:
+    try:
+        guess = int(input("Guess: "))
+    except ValueError:
+        continue
+
+    if guess > number:
+        print("Too high!")
+    elif guess < number:
+        print("Too low!")
+    if guess == number:
+        print("Just right!")
+        break
+
+
+"""
+# THINGS LEARNT
+- sometime 2 while loops are what's necessary 
+- ValueErrors occur when there's a type conversion 
+- a little bit about randint
+"""
+        
+
+
+
+
+
+
+
+
+
+
+
