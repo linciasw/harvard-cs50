28d79b0fad32e3cb9463d2aa59e60abc838f49af
Lincia Walters <lincia.walters@hotmail.com>
2026-08-23 09:34:36 -0400

    figlet.py completed

--- 4 lecture four - libraries/problem sets/figlet.py
+++ 4 lecture four - libraries/problem sets/figlet.py
@@ -6,7 +6,7 @@
 # documentation for sys
 # https://docs.python.org/3/library/sys.html
 
-from pyfiglet import Figlet
+from pyfiglet import Figlet, FontNotFound
 import sys
 import random 
 
@@ -17,30 +17,43 @@ figlet = Figlet()
 input = input("Input: ")
 
 
+print(len(sys.argv))
+print(sys.argv)
 
 
-if len(sys.argv) == 1:
-    figlet.setFont(font=str(random.choice(figlet.getFonts())))
-    print(figlet.renderText(input))
-elif len(sys.argv) == 2:
-    # font = sys.argv[2]
-    figlet.setFont(font=sys.argv[1])
-    print(figlet.renderText(input))
-elif len(sys.argv) == 3 and sys.argv[1] != "-f" or sys.argv[1] != "-font":
+# if len(sys.argv) == 1:
+#     figlet.setFont(font=str(random.choice(figlet.getFonts())))
+#     print(figlet.renderText(input))
+# elif len(sys.argv) == 2:
+#     # font = sys.argv[2]
+#     figlet.setFont(font=sys.argv[1])
+#     print(figlet.renderText(input))
+# elif "-f" or "-font" in sys.argv:
+
+
+try:
+    if len(sys.argv) == 1:
+        figlet.setFont(font=str(random.choice(figlet.getFonts())))
+        print(figlet.renderText(input))
+    elif len(sys.argv) > 1 and "-f" in sys.argv or "--font" in sys.argv:
+        figlet.setFont(font=sys.argv[2])
+        print(figlet.renderText(input))
+except FontNotFound as error:
+    print(f"Error: The font '{error}' could not be found.")
+else:
     sys.exit
 
 
-# else:
-#     sys.exit
 
+"""
+# THINGS I LEARNT
+- because sys.argv is a list, checking for the presense of a argument in the list 
+is a much better idea than creating a conditional to check if an argument is at an index 
+and then doing something
+- you have to import the exception as well, if you're only importing one function from the module 
+at first
 
-print(len(sys.argv))
-# print(sys.argv[1])
-# print(sys.argv[2])
-# print(sys.argv[3])
-
-
-
+"""
 
 
 
