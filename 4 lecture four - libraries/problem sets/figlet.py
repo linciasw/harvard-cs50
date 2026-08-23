d95f6e0d124b47ea31d35846d1c41d5721798ac8
Lincia Walters <lincia.walters@hotmail.com>
2026-08-23 09:37:40 -0400

    docstring added to figlet.py

--- 4 lecture four - libraries/problem sets/figlet.py
+++ 4 lecture four - libraries/problem sets/figlet.py
@@ -1,3 +1,18 @@
+"""
+In a file called figlet.py, implement a program that:
+
+Expects zero or two command-line arguments:
+Zero if the user would like to output text in a random font.
+Two if the user would like to output text in a specific font, 
+in which case the first of the two should be -f or --font, 
+and the second of the two should be the name of the font.
+Prompts the user for a str of text.
+Outputs that text in the desired font.
+If the user provides two command-line arguments and the first is not -f or --font or the second is not the name of a font, 
+the program should exit via sys.exit with an error message.
+
+"""
+
 # find out why -f not working if i put in the command line
 # documentation for figlet
 # https://www.figlet.org/figlet-man.html
@@ -8,8 +23,7 @@
 
 from pyfiglet import Figlet, FontNotFound
 import sys
-import random 
-
+import random
 
 figlet = Figlet()
 
@@ -44,7 +58,6 @@ else:
     sys.exit
 
 
-
 """
 # THINGS I LEARNT
 - because sys.argv is a list, checking for the presense of a argument in the list 
@@ -54,9 +67,3 @@ and then doing something
 at first
 
 """
-
-
-
-
-
-
