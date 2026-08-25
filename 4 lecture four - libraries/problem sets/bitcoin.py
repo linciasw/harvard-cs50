c35631df1756194efefd29b92358d83efaacaab4
Lincia Walters <lincia.walters@hotmail.com>
2026-08-25 06:19:54 -0400

    bitcoin.py completed

--- 4 lecture four - libraries/problem sets/bitcoin.py
+++ 4 lecture four - libraries/problem sets/bitcoin.py
@@ -1,1 +1,40 @@
-# 
+# https://pro.coincap.io/dashboard
+# https://pro.coincap.io/api-docs/
+
+
+import sys
+import requests
+
+
+
+API_KEY = "acc97ed45d648a8fd5ac30c1e039c1a7420408e9b48076f38032f91f25d47475"
+
+
+def main():
+
+
+    bitcoin = float(sys.argv[1])
+
+
+    response= requests.get(
+        "https://rest.coincap.io/v3/price/bysymbol/BTC",
+        headers={"Authorization": f"Bearer {API_KEY}"}
+    )
+
+
+    data = response.json()
+    price = float(data["data"][0])
+
+    total = bitcoin * price 
+    print(f"$ {total:,.2f}")
+
+    # print("BTC:")
+    # print(response.status_code)
+    # print(response.text)
+
+
+
+
+
+
+main()
