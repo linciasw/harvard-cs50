1b63dec293915e929eae03b68d3f405951d41bb0
Lincia Walters <lincia.walters@hotmail.com>
2026-08-25 13:06:07 -0400

    botcoin.py updated

--- 4 lecture four - libraries/problem sets/bitcoin.py
+++ 4 lecture four - libraries/problem sets/bitcoin.py
@@ -37,7 +37,17 @@ API_KEY = "acc97ed45d648a8fd5ac30c1e039c1a7420408e9b48076f38032f91f25d47475"
 def main():
 
 
+    print(sys.argv)
+
+    if len(sys.argv) == 1:
+        print("Missing command-line argument")
+        sys.exit()
+
+
+
     bitcoin = float(sys.argv[1])
+
+
 
 
     response= requests.get(
