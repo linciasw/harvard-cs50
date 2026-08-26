538be6e23c0cd9e6633a49fb86f5f2c5ed8d94f7
Lincia Walters <lincia.walters@hotmail.com>
2026-08-26 18:32:32 -0400

    reearched weather-cli a little bit

---
+++ 4 lecture four - libraries/api exercises/weather-cli.py
@@ -0,0 +1,43 @@
+'''
+1. 🌤️ Weather CLI
+API: Open-Meteo
+API Key: Not required
+Difficulty: ⭐⭐
+
+Goal
+Build a command-line weather program.
+
+python weather.py Port_of_Spain
+
+Example output:
+Port of Spain
+Temperature: 29°C
+Wind: 18 km/h
+Weather: Partly cloudy
+
+Practice:
+    requests.get()
+    Query parameters with params=
+    JSON
+    Dictionaries
+    Lists
+    Command-line arguments
+    Reading API documentation
+
+
+https://open-meteo.com/en/docs?hourly=&location_mode=csv_coordinates
+https://www.geopythontutorials.com/notebooks/openmeteo_weather_forecast.html#get-daily-forecast
+
+
+need to use geocoding to get the latitude and longitude from place name
+https://nominatim.org/release-docs/latest/library/Getting-Started/#__tabbed_1_2
+'''
+
+
+def main():
+
+
+
+
+
+main()
