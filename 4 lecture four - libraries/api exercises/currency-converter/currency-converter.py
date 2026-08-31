45770fffaaaf414b8f943c81fadf117a97a6b124
Lincia Walters <lincia.walters@hotmail.com>
2026-08-31 09:47:36 -0400

    currency-converter.py created

---
+++ 4 lecture four - libraries/api exercises/currency-converter/currency-converter.py
@@ -0,0 +1,41 @@
+"""
+============================================================
+2. 💱 Currency Converter
+============================================================
+
+API:
+Frankfurter or ExchangeRate API
+
+API Key:
+Depends on the API
+
+Difficulty:
+⭐⭐½
+
+
+Goal:
+Build a currency conversion program.
+
+Example:
+python convert.py USD TTD 100
+
+Output:
+100 USD = 678.42 TTD
+
+
+Support different currencies:
+
+python convert.py USD EUR 100
+python convert.py GBP TTD 50
+
+
+Practice:
+- URL parameters
+- JSON
+- Dictionaries
+- Floats
+- Command-line arguments
+- Invalid currency handling
+- Reading and understanding API documentation
+
+"""
