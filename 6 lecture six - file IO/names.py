3abe795f32598cd19a1fd3cee50a97352a0b70eb
Lincia Walters <lincia.walters@hotmail.com>
2026-09-02 17:00:23 -0400

    lecture 6 continued

--- 6 lecture six - file IO/names.py
+++ 6 lecture six - file IO/names.py
@@ -1,5 +1,6 @@
 # FROM LECTURE
-# REACHED 15:30
+# REACHED 20:14
+# READ DOCUMENTATION ON FILE I/O
 
 
 
@@ -25,7 +26,7 @@
 # the equivalent of double clicking on the gui
 # documentation: https://docs.python.org/3/library/functions.html#open
 
-name = input("What's your name? ")
+# name = input("What's your name? ")
 
 # file = open("names.txt", "w")
 # file.write(f"{name}\n") 
@@ -70,7 +71,46 @@ name = input("What's your name? ")
 
 # with
 # sometimes we forget to close files using the .close() function
-# tells python to open and close file
-with open("names.txt", "a") as file:
-    file.write(f"{name}\n") 
+# with tells python to open and close file
+# with open("names.txt", "a") as file:
+#     file.write(f"{name}\n") 
+
+# if there's no more code after the with block, python will automatically close the file for you 
+
+
+
+
+
+# PROGRAM TO OPEN FILE
+# with open("names.txt", "r") as file:
+#     lines = file.readlines()
+
+# for line in lines:
+#     print("hello,", line) 
+    # the output here is:
+    # hello, Hermione
+
+    # hello, Harry
+
+    # the names in the file has a new line at the end (see code above) and print adds in 
+    # a new line so it's printing 2 new lines
+
+
+    # we add an .rstrip() function to strip the new line space at the right of the line in the file
+    # to allow print() to add its new line 
+# for line in lines:
+#     print("hello,", line.rstrip())
+
+
+# we can shorten everything above by doing:
+with open("nJames.txt", "r") as file:
+    for line in file:
+        print("hello,", line.rstrip())
+
+
+
+
+
+
+
 
