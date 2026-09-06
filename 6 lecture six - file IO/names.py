776b26b161812d26861d41ce4421baf40d0aecca
Lincia Walters <105956719+linciasw@users.noreply.github.com>
2026-09-05 22:08:00 -0400

    Update names.py

--- 6 lecture six - file IO/names.py
+++ 6 lecture six - file IO/names.py
@@ -5,7 +5,7 @@
 
 
 # file I/O within the context of programming, 
-# is all about writing code than can read from or write to, files themselves
+# is all about writing code that can read from or write to, files themselves
 
 
 # in the below, everytime the names are printed,
